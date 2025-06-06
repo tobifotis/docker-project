9435677e18e271ecff708a41e8a7ec7dc9f96ffe
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-06 08:53:59 -0500

    Create Simple API with Node JS and Express JS framework

--- user-service-api/index.js
+++ user-service-api/index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-    const data = [{
-        name: 'Cristiano Ronaldo',
-        Position: 'Striker',
-        Status: 'Goat'
-    }];
-    res.send(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
+    const message = `
+        <h2>🚧 Test Endpoint</h2>
+        <p>This endpoint is working correctly and is currently used for testing purposes only.</p>
+    `;
+    res.send(message);
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
